ed69c33d225fcccdb05d383fdf8d5536910b94e4
Dawid Haładus <alerinpoland@gmail.com>
2024-03-24 18:14:12 +0100

    Dodaj pliki projektów.

---
+++ Source/Zonit.Core.Attributes/Validations/NoSpecialCharactersAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Zonit.Core.Attributes.Validations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public partial class NoSpecialCharactersAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not string stringValue)
+            return false;
+
+        if (SearchSpecialCharRegex().IsMatch(stringValue))
+        {
+            ErrorMessage = "Special characters cannot be used.";
+            return false;
+        }
+
+        return true;
+    }
+
+    [GeneratedRegex(@"[^\w\s]+")]
+    private static partial Regex SearchSpecialCharRegex();
+}
---
+++ Source/Zonit.Core.Attributes/Validations/WordsCountAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zonit.Core.Attributes.Validations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class WordsCountAttribute(int _minWords, int _maxWords) : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not string stringValue)
+            return false;
+
+        string[] words = stringValue.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int wordCount = words.Length;
+        if (wordCount < _minWords || wordCount > _maxWords)
+        {
+            ErrorMessage = $"Name must contain between {_minWords} and {_maxWords} words.";
+            return false;
+        }
+
+        return true;
+    }
+}
