dc1fb3dae430495ad4f33c2d56dace8b74f4cfd8
Dawid Haładus <alerinpoland@gmail.com>
2024-04-15 22:20:21 +0200

    Dodano obsługę EntityFrameworkCore i nowy projekt bazy danych

---
+++ Source/Zonit.Core.Databases/SqlServer/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Zonit.Abstractions.Options;
+
+namespace Zonit.Core.Databases.SqlServer.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDbContextSqlServerFactory<T>(this IServiceCollection services) where T : DbContext
+    {
+        services.AddDbContextFactory<T>((serviceProvider, options) =>
+        {
+            var databaseOptions = serviceProvider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+            options.UseSqlServer($"Server={databaseOptions.Server};Database={databaseOptions.Name};user={databaseOptions.User};Password={databaseOptions.Password};{databaseOptions.Parameters}");
+        });
+
+        return services;
+    }
+}
