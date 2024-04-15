8c33c0e6a4c46bffbaf8d8297d992bf4ce846346
Dawid Haładus <alerinpoland@gmail.com>
2024-04-15 22:36:46 +0200

    Update 0.0.5: Aktualizacja zależności i dodanie konfiguracji bazy danych
    Zmieniono wersję `AssemblyVersion` z `0.0.4` na `0.0.5` w pliku `Directory.Packages.props`. Dodano nowe zależności `Microsoft.Extensions.Configuration.Abstractions` i `Microsoft.Extensions.Configuration.Binder` w plikach `Directory.Packages.props` i `Zonit.Core.Databases.csproj`. Usunięto zależności `Microsoft.EntityFrameworkCore`, `Microsoft.EntityFrameworkCore.Abstractions` i `Microsoft.EntityFrameworkCore.SqlServer` z tych samych plików.
    
    Dodano import `Microsoft.Extensions.Configuration` oraz nową metodę `AddDatabase` w pliku `ServiceCollectionExtensions.cs`, która dodaje konfigurację bazy danych do usług.

--- Source/Zonit.Core.Databases/SqlServer/DependencyInjection/ServiceCollectionExtensions.cs
+++ Source/Zonit.Core.Databases/SqlServer/DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Zonit.Abstractions.Options;
+using Microsoft.Extensions.Configuration;
 
 namespace Zonit.Core.Databases.SqlServer.DependencyInjection;
 
@@ -14,6 +15,22 @@ public static class ServiceCollectionExtensions
             var databaseOptions = serviceProvider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
             options.UseSqlServer($"Server={databaseOptions.Server};Database={databaseOptions.Name};user={databaseOptions.User};Password={databaseOptions.Password};{databaseOptions.Parameters}");
         });
+
+        return services;
+    }
+
+    /// <summary>
+    /// Inicjalize Database, config etc.
+    /// Only register Program.cs
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddDatabase(this IServiceCollection services)
+    {
+        services.AddOptions<DatabaseOptions>()
+            .Configure<IConfiguration>(
+                (options, configuration) =>
+                    configuration.GetSection("Database").Bind(options));
 
         return services;
     }
