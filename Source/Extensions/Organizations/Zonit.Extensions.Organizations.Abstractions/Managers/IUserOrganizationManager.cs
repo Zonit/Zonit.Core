76b7583da41c95b09e22d36fbe34b4414186c2a2
Dawid Haładus <alerinpoland@gmail.com>
2024-05-29 00:53:28 +0200

    Migration organization to new repo

--- Source/Extensions/Organizations/Zonit.Extensions.Organizations.Abstractions/Managers/IUserOrganizationManager.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Zonit.Extensions.Organizations;
-
-public interface IUserOrganizationManager
-{
-    public Task<WorkspaceModel> InicjalizeAsync();
-    public Task<IReadOnlyCollection<OrganizationModel>?> GetOrganizationsAsync();
-    public Task<WorkspaceModel?> SwitchOrganizationAsync(Guid organizationId);
-}
